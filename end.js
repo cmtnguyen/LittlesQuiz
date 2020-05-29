87eba24237a5061701032e5e32c50fcfebf845d4
chillayx <66109485+chillayx@users.noreply.github.com>
2020-05-29 13:39:35 -0700

    Update end.js

--- end.js
+++ end.js
@@ -24,6 +24,6 @@ saveHighScore = (e) => {
     highScores.splice(5);
 
     localStorage.setItem("highScores", JSON.stringify(highScores));
-    window.location.assign("/LittlesQuiz/highscores.html");
+    return window.location.assign("/LittlesQuiz/highscores.html");
 };
 
